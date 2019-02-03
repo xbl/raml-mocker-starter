c80051fd90745d0f59f0dfc88c74a23814697049
xbl <blxie@toughtworks.com>
2019-02-03 11:07:41 +0800

    feat(article): 参数化示例

--- test/article.spec.js
+++ test/article.spec.js
@@ -14,3 +14,12 @@ it('从文章列表到文章详情', async () => {
   const { data: detail } = await getDetail({ id: articleId });
   assert.equal(detail.title, '提升家里整体格调的小物件');
 });
+
+describe('参数化测试示例', () => {
+  const params = {id: 'A00002', title: '提升家里整体格调的小物件'}
+  it(`根据文章id "${params.id}" 获取文章详情，文章的标题应为 "${params.title}"`, async () => {
+    const getDetail = loadApi('文章详情');
+    const { data: detail } = await getDetail({ id: params.id });
+    assert.equal(detail.title, params.title);
+  });
+});
